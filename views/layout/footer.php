567fc3a4f2fa54a100a6523e237d33cdef688808
emypm90 <emypm90@gmail.com>
2022-11-29 16:32:05 -0300

    inicial commit

---
+++ views/layout/footer.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+
+			<!-- PIE DE PÁGINA -->
+			<footer id="footer">
+				<p>Desarrollado por Emiliano Pérez Méndez &copy; <?= date('Y') ?></p>
+			</footer>
+		</div>
+	</body>
+</html>
